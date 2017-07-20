042d78d7d261e5467093501fe87b3f1979249cad
jingdong521 <563674780@qq.com>
2017-07-20 16:22:56 +0800

    1

--- 小视频录制/ViewController.m
+++ 小视频录制/ViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "KZVideoViewController.h"
+#import "KZVideoPlayer.h"
+@interface ViewController ()<KZVideoViewControllerDelegate>
+{
+    KZVideoModel *_videoModel;
+}
+@property (weak, nonatomic) IBOutlet UILabel *sizeLable;
+@property (weak, nonatomic) IBOutlet UIView *videoView;
 @end
 
 @implementation ViewController
@@ -18,8 +23,31 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
+- (IBAction)startRecord:(id)sender {
+    
+    KZVideoViewController *videoVC = [[KZVideoViewController alloc] init];
+    videoVC.delegate = self;
+    [videoVC startAnimationWithType:KZVideoViewShowTypeSmall];
+}
+- (void)videoViewController:(KZVideoViewController *)videoController didRecordVideo:(KZVideoModel *)videoModel {
+    _videoModel = videoModel;
+    NSError *error = nil;
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSDictionary *attri = [fm attributesOfItemAtPath:_videoModel.videoAbsolutePath error:&error];
+    if (error) {
+        NSLog(@"error:%@",error);
+    }
+    else {
+        self.sizeLable.text = [NSString stringWithFormat:@"视频总大小:%.0fKB",attri.fileSize/1024.0];
+    }
+    NSURL *videoUrl = [NSURL fileURLWithPath:_videoModel.videoAbsolutePath];
+    KZVideoPlayer *player = [[KZVideoPlayer alloc] initWithFrame:self.videoView.bounds videoUrl:videoUrl];
+    [self.videoView addSubview:player];
+    
+}
+- (void)videoViewControllerDidCancel:(KZVideoViewController *)videoController {
+    NSLog(@"没有录到视频");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
